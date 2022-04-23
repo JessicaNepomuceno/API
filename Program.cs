2070fc3afaa61a5607b6320297e0b9be2d4e0e09
Jessica Nepomuceno <jessica_alves_nep@hotmail.com>
2022-04-23 15:20:29 -0400

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+HttpClient repositories = new HttpClient();
+repositories.BaseAddress = new Uri("https://api.github.com");
+repositories.DefaultRequestHeaders.Add("User-Agent", "request");
+repositories.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+repositories.DefaultRequestHeaders.Add("Authorization", "token ");
+
+//var username = "JessicaNepomuceno";
+var content = await repositories.GetAsync($"user/repos");
+var response = await content.Content.ReadAsStringAsync();
+
+var repoGitHub = JsonConvert.DeserializeObject<API.ListaRepositorioGitHub[]>(response);
+
+foreach (var item in repoGitHub)
+{
+    Console.WriteLine(item.Name);
+}
